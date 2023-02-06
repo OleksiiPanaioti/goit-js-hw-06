6edaac597759dbaa276c141af6e403f7f6191fa6
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-06 12:29:13 +0200

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsEl = ingredients.map(ingredient => {
+  const listCreate = document.createElement("li");
+  listCreate.textContent = ingredient;
+  listCreate.classList.add('item');
+  return listCreate;
+})
+
+
+
+const ulId = document.querySelector('#ingredients');
+console.log(ulId)
+
+ulId.append(...ingredientsEl);
