5f85276f23fa04cdd520876260ac69d987e8fd88
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-12 13:02:07 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const refs = {
+  button: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+  span: document.querySelector('.color'),
+};
+
+
+refs.button.addEventListener('click', onClickBodyColorChange);
+
+function onClickBodyColorChange(event) {
+  const randomColor = getRandomHexColor();
+  refs.body.style.backgroundColor = randomColor;
+  refs.span.textContent = randomColor
 }
