28ef69d06f3fe5cdba43bba95cc8839e022f0dab
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-12 12:34:37 +0200

    task 8 done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
 
+const form = document.querySelector('.login-form');
+
+form.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+    const email = formElements.email;
+    const password = formElements.password;
+
+  if (email.value === "" || password.value === "") {
+    return console.log("ти шо дикий??? треба заповнити всі поля!!!!");
+  } else {  const formElResult = {
+      email: email.value,
+      password:  password.value ,
+  }
+         console.log(formElResult); 
+  }
+
+
+    
+  event.currentTarget.reset();
+}
