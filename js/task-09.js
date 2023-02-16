761be46867c89f02cf5c3a0d18ec967ab37567a6
OleksiiPanaioti <cavedzgn@gmail.com>
2023-02-16 08:19:38 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,6 +3,8 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
 const refs = {
   button: document.querySelector('.change-color'),
   body: document.querySelector('body'),
